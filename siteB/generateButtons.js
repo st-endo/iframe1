6ef2f8ba5283f3dac9eb4c1ff84bade7ff87db94
st-endo <y.endo@stands.ai>
2023-08-05 09:07:59 +0900

    動的要素にもbox表示

--- siteB/generateButtons.js
+++ siteB/generateButtons.js
@@ -1,10 +1,19 @@
 function generateButtons() {
-  for (let i = 1; i <= 1000; i++) {
+  let i = 1;
+
+  const intervalId = setInterval(function () {
+    if (i > 1000) {
+      clearInterval(intervalId);
+      return;
+    }
+
     const button = document.createElement("button");
     button.id = "btn" + i;
     button.textContent = "btn" + i;
     document.body.appendChild(button);
-  }
+
+    i++;
+  }, 10);
 }
 
 export default generateButtons;
--- siteB/main.js
+++ siteB/main.js
@@ -4,37 +4,54 @@ window.addEventListener(
   "message",
   function (event) {
     boxes = event.data.boxes;
+  },
+  false
+);
 
-    boxes.forEach((box) => {
-      const specialElement = document.querySelector(box.target);
-      if (specialElement) {
-        specialElement.addEventListener("mouseenter", function () {
-          const rect = specialElement.getBoundingClientRect();
+document.body.addEventListener(
+  "mouseover",
+  function (event) {
+    console.log(event);
+    // イベントが発生した要素がボタンであるか確認
+    if (event.target.tagName.toLowerCase() === "button") {
+      // boxを取得
+      const box = boxes.find((box) => box.target === "#" + event.target.id);
+      if (box) {
+        const rect = event.target.getBoundingClientRect();
+        window.parent.postMessage(
+          {
+            action: "show",
+            frameId: "frame1",
+            rect: rect,
+            box: box,
+          },
+          "*"
+        );
+      }
+    }
+  },
+  false
+);
+
+document.body.addEventListener(
+  "mouseout",
+  function (event) {
+    // イベントが発生した要素がボタンであるか確認
+    if (event.target.tagName.toLowerCase() === "button") {
+      const box = boxes.find((box) => box.target === "#" + event.target.id);
+      if (box) {
+        setTimeout(function () {
           window.parent.postMessage(
             {
-              action: "show",
-              frameId: event.data.frameId,
-              rect: rect,
-              box: box,
+              action: "hide",
+              frameId: "frame1",
+              boxId: box.boxId,
             },
             "*"
           );
-        });
-
-        specialElement.addEventListener("mouseleave", function () {
-          setTimeout(function () {
-            window.parent.postMessage(
-              {
-                action: "hide",
-                frameId: event.data.frameId,
-                boxId: box.boxId,
-              },
-              "*"
-            );
-          }, 100);
-        });
+        }, 100);
       }
-    });
+    }
   },
   false
 );
